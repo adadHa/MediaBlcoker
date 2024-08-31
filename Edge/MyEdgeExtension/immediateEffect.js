66ecaa5cf483168e6cfaa8f8b54d1084f90767d7
adadi <adadi@yatmal.co.il>
2024-08-31 23:39:40 +0300

    aa

--- Edge/MyEdgeExtension/immediateEffect.js
+++
@@ -1,8 +0,0 @@
-let styleElement = null;
-
-
-if (!styleElement) {
-    styleElement = document.createElement('style');
-    styleElement.textContent = 'img { filter: opacity(0) !important; }';
-    document.documentElement.insertBefore(styleElement, document.documentElement.firstChild);
-  }
